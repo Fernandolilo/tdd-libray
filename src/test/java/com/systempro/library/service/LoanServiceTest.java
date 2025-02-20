b0e40017b2ee5f66d1eab3996b63731b4c841f58
fernandolilo <fernando.sistenpro@outlook.com>
2025-02-20 20:50:18 -0300

    Iniciado testes em LoanServiceTest

---
+++ src/test/java/com/systempro/library/service/LoanServiceTest.java
@@ -0,0 +1,19 @@
+package com.systempro.library.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class LoanServiceTest {
+
+	@Test
+	@DisplayName("Deve Salvar um emprestimo")
+	public void saveLoanTest() {
+		
+		
+	}
+}
