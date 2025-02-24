64efc49ef8f27f3d39a8fc22e32b0c91200a21c0
fernandolilo <fernando.sistenpro@outlook.com>
2025-02-24 07:21:50 -0300

    Iniciado filtro de busca de emprestimos em LoanServiceTest

--- src/main/java/com/systempro/library/dto/LoanFilterDTO.java
+++ src/main/java/com/systempro/library/dto/LoanFilterDTO.java
@@ -1,10 +1,14 @@
 package com.systempro.library.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoanFilterDTO {
 
 	private String isbn;
