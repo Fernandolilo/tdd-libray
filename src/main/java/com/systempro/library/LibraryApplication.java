10bcc1e2e2ffa0cafba1cb5ff74339a3fcc61b0d
fernandolilo <fernando.sistenpro@outlook.com>
2025-02-24 09:05:58 -0300

    Iniciado o agendamento de tarefas com o spring

--- src/main/java/com/systempro/library/LibraryApplication.java
+++ src/main/java/com/systempro/library/LibraryApplication.java
@@ -1,10 +1,14 @@
+
 package com.systempro.library;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+//@EnableScheduling agendametno de tarefas
+@EnableScheduling
 @SpringBootApplication
 public class LibraryApplication {
 	
@@ -12,7 +16,18 @@ public class LibraryApplication {
 	public ModelMapper mapper() {
 		return new ModelMapper();
 	}
+	
+	//cron vem ce conologico ou sejatempo
+	/*site da cronmaker para gerar os parametros cron
+	 * 
+	 * http://www.cronmaker.com/;jsessionid=node02bro7auu4cd31t3tt77fc01eb2146780.node0?0
+	 * */
 
+	@Scheduled(cron = "0 5 9 ? * MON-FRI")
+	public void testAgendamentoTarefaz() {
+		System.out.println("AGENDAMENTO DE TAREFAS FUNCIONANDO COM SUCESSO");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
